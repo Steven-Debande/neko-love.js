c2b081fc364cd9cf4aa906ad7cb6f6296a3c9449
Steven-Debande <51061507+Steven-Debande@users.noreply.github.com>
2019-11-14 16:25:24 +0100

    Update Base.js

--- src/Base.js
+++ src/Base.js
@@ -3,17 +3,15 @@
 const wump = require("wumpfetch");
 const base = "https://neko-love.xyz/api/v1/";
 
-module.exports = (endpoint) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const data = await wump(endpoint === "endpoint" ? base : `${base}${endpoint}`).send();
-            if (data.statusCode !== 200) {
-                reject(new Error(data));
-            } else {
-                resolve(endpoint === "endpoint" ? data.parse().endpoint : data.parse().url);
-            }
-        } catch (error) {
-            reject(new Error(error));
+module.exports = async (endpoint) => {
+    try {
+        const data = await wump(endpoint === "endpoint" ? base : `${base}${endpoint}`).send();
+        if (data.statusCode !== 200) {
+            return new Error(data);
+        } else {
+            return endpoint === "endpoint" ? data.parse().endpoint : data.parse().url;
         }
-    });
+    } catch (error) {
+        return new Error(error);
+    }
 };
