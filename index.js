300421220659766f934638162d6edb4f4313ea39
Steven-Debande <51061507+Steven-Debande@users.noreply.github.com>
2019-12-10 13:36:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 "use strict";
 
-module.exports = {
-    Base: require("./src/Base"),
-    version: require("./package.json").version
+const { get } = require("https");
+const base = "https://neko-love.xyz/api/v1/";
+
+module.exports = async (endpoint) => {
+    const url = new URL(endpoint === "endpoint" ? base : `${base}${endpoint}`);
+    try {
+        return await getContext(url.toString(), endpoint === "endpoint" ? true : false);
+    } catch (error) {
+        return console.error(`Error ${error.message}`);
+    }
 };
+
+function getContext(url, type) {
+    return new Promise((resolve, reject) =>  {
+        get(url, (res) => {
+            const { statusCode } = res;
+            if (statusCode !== 200) {
+                res.resume();
+                reject(`Error Request Status Code ${statusCode}`);
+            }
+            res.setEncoding("utf8");
+            let rawData = "";
+            res.on("data", (chunk) => {
+                rawData += chunk;
+            });
+            res.on("end", () => {
+                try {
+                    const parseData = JSON.parse(rawData);
+                    resolve(type ? parseData.endpoint : parseData.url);
+                } catch (error) {
+                    reject(`Error ${error.message}`);
+                }
+            });
+        }).on("error", (err) => {
+            reject(`Error ${error.message}`);
+        });
+    });
+}
